e30006298dcf9a1d96582f70ee16cb174708e16f
Hajime Nagisa <jyoichirou99@gmail.com>
2024-06-05 14:31:13 +0900

    feat: a script to rebuild schema
    This script nukes databases in development environment. Since ElectricSQL only accepts additive schema change, Resetting the entire databases is necessary during early development.

---
+++ db/rebuild-schema.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# This script nukes databases in development environment. 
+# Since ElectricSQL only accepts additive schema change, Resetting the entire databases is necessary during early development.
+
+# check whether macOS or others
+if [[ "$(uname)" != "Darwin" ]]; then
+  echo 'This script only works with macOS'
+  exit 1
+fi
+
+docker compose -f docker/compose.dev.yml down 
+docker volume rm potrin_dev_pg_data 
+docker compose -f docker/compose.dev.yml up -d 
+
+# wait until the containers are ready
+while true; do 
+  log=$(docker compose -f docker/compose.dev.yml logs --tail=1)
+  search_string='LOG:  logical replication apply worker for subscription "postgres_1" has started'
+  if echo "$log" | grep -q "$search_string"; then
+    break
+  fi
+  sleep 1
+done
+
+pnpm run drizzle push --dialect='postgresql' --schema='./db/schema.ts' --url=postgresql://postgres:proxy_password@localhost:65432
+
+export PGPASSWORD='proxy_password'
+psql -h localhost -p 65432 -U postgres -a -f db/electrify.sql
+
+pnpm exec electric-sql generate
+
+# cleanup app data
+rm -rf ~/Library/"Application Support"/com.potrin.dev/*
---
+++ db/schema.ts
@@ -0,0 +1,31 @@
+import { pgTable, text, timestamp, uuid, index, boolean } from 'drizzle-orm/pg-core';
+
+export const card = pgTable('cards', {
+  id: uuid('id').primaryKey(),
+  thread: uuid('thread'),
+  prev_card: uuid('prev_card'),
+  content: text('content'),
+  created_at: timestamp('created_at'),
+  updated_at: timestamp('updated_at'),
+}, (cards) => {
+  return {
+    thread_idx: index("thread_idx").on(cards.thread),
+    prev_card_idx: index("prev_card_idx").on(cards.prev_card)
+  }
+});
+
+export const thread = pgTable('threads', {
+  id: uuid('id').primaryKey(),
+  parent_thread: uuid('parent_thread'),
+  prev_thread: uuid('prev_thread'),
+  title: text('title'),
+  created_at: timestamp('created_at'),
+  updated_at: timestamp('updated_at'),
+  deleted: boolean('deleted'),
+}, (threads) => {
+  return {
+    parent_thread_idx: index("parent_thread_idx").on(threads.parent_thread),
+    prev_thread_idx: index("prev_thread_idx").on(threads.prev_thread)
+  }
+});
+
