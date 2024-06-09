ecdd6515c5dd3506c8edeb7138286182e9501e23
Hajime Nagisa <jyoichirou99@gmail.com>
2024-06-09 21:40:51 +0900

    feat: electric instance initialization

---
+++ src/lib/DataAccess/electric.ts
@@ -0,0 +1,22 @@
+import { electrify } from "electric-sql/tauri";
+import { ElectricClient } from "electric-sql/client/model";
+import { schema } from "../../generated/client";
+import Database from "@tauri-apps/plugin-sql";
+import { ok } from "neverthrow";
+import { execAsyncThrowable } from "$lib/Utils/neverthrow-utils";
+
+const config = {
+  url: "http://localhost:5133",
+  debug: false,
+};
+
+export let electric: undefined | ElectricClient<typeof schema>;
+
+export function init() {
+  return execAsyncThrowable(() => Database.load("sqlite:electric.db"))
+    .andThen((db) => ok(Object.assign(db, { name: "electric.db" })))
+    .andThen((db) => execAsyncThrowable(() => electrify(db, schema, config)))
+    .map((e) => {
+      electric = e;
+    });
+}
