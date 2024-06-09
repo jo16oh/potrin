e235b6229fa22634e20bbf6453cf68c3d5b875c6
Hajime Nagisa <jyoichirou99@gmail.com>
2024-06-09 21:39:49 +0900

    test: electric instance for test

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -6,13 +6,14 @@ import {
 import { exec } from "node:child_process";
 import util from "node:util";
 
-let env: StartedDockerComposeEnvironment | null = null;
+let env: StartedDockerComposeEnvironment | undefined = undefined;
 const targetLog =
   'LOG:  logical replication apply worker for subscription "postgres_1" has started';
 const commands = `
 pnpm run drizzle push --dialect='postgresql' --schema='./db/schema.ts' --url=postgresql://postgres:proxy_password@localhost:65433 &&
 export PGPASSWORD='proxy_password' &&
-psql -h localhost -p 65433 -U postgres -a -f db/electrify.sql
+psql -h localhost -p 65433 -U postgres -a -f db/electrify.sql &&
+pnpm exec electric-sql generate --proxy postgresql://postgres:proxy_password@localhost:65433/postgres -s http://localhost:5134
 `;
 
 export async function setup() {
