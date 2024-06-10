752643d2ba8883bb845e4a894dd76a80d3b00c00
Hajime Nagisa <jyoichirou99@gmail.com>
2024-06-10 10:28:19 +0900

    chore: add global objects to eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,21 @@
 // @ts-check
-
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
 import eslintPluginSvelte from "eslint-plugin-svelte";
+import globals from "globals";
 
 export default [
   eslint.configs.recommended,
   eslintConfigPrettier,
   ...tseslint.configs.strict,
   ...eslintPluginSvelte.configs["flat/prettier"],
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
 ];
