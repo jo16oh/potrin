e9ce0a7393b9010b882885c5988c9ea26f3c04a0
Hajime Nagisa <jyoichirou99@gmail.com>
2024-06-12 22:51:15 +0900

    feat: include cards in the thread tree

--- src/lib/Models/queries/getThreadTree.sql.ts
+++ src/lib/Models/queries/getThreadTree.sql.ts
@@ -1,10 +1,18 @@
 import { sql } from "$lib/Utils/utils";
 
+type Card = {
+  id: string;
+  content: string;
+  thread: string;
+  fractional_index: string;
+};
+
 export type ThreadTree = {
   id: string;
   title: string;
   fractional_index: string;
   parent_thread: ThreadTree;
+  cards: Card[];
   child_threads?: ThreadTree[];
 };
 
@@ -12,20 +20,37 @@ export const query = sql`
 WITH RECURSIVE thread_tree AS MATERIALIZED (
 	SELECT id, title, parent_thread, fractional_index, 0 AS depth
 	FROM threads
-	WHERE id = ?
+	WHERE id = ? AND deleted = false
 	UNION ALL
 	SELECT child.id, child.title, child.parent_thread, child.fractional_index, parent.depth + 1
 	FROM thread_tree AS parent
 	JOIN threads AS child ON parent.id = child.parent_thread
-	WHERE parent.depth < 7
+	WHERE parent.depth < 7 AND deleted = false
 	ORDER BY depth DESC
+),
+thread_cards AS MATERIALIZED (
+  SELECT cards.id, cards.content, cards.thread, cards.fractional_index
+  FROM cards
+	JOIN thread_tree ON cards.thread = thread_tree.id
+  WHERE cards.thread = thread_tree.id AND deleted = false
+	ORDER BY cards.fractional_index ASC, cards.id ASC
 ),
 d6 AS (
 	SELECT
 		json_object(
-		'id', id,
-		'title', title,
-		'fractional_index', fractional_index
+			'id', id,
+			'title', title,
+			'fractional_index', fractional_index,
+			'cards', (
+				SELECT json_group_array(json_object(
+					'id', id,
+					'content', content,
+					'thread', thread,
+					'fractional_index', fractional_index
+				) ORDER BY fractional_index ASC, id ASC)
+				FROM thread_cards
+				WHERE thread_cards.thread = t6.id
+			)
 		) AS json,
 		fractional_index,
 		parent_thread
@@ -43,6 +68,16 @@ d5 AS (
 				SELECT json_group_array(json(json))
 				FROM d6
 				WHERE d6.parent_thread = t5.id
+			),
+			'cards', (
+				SELECT json_group_array(json_object(
+					'id', id,
+					'content', content,
+					'thread', thread,
+					'fractional_index', fractional_index
+				) ORDER BY fractional_index ASC, id ASC)
+				FROM thread_cards
+				WHERE thread_cards.thread = t5.id
 			)
 		) AS json,
 		fractional_index,
@@ -61,6 +96,16 @@ d4 AS (
 				SELECT json_group_array(json(json))
 				FROM d5
 				WHERE d5.parent_thread = t4.id
+			),
+			'cards', (
+				SELECT json_group_array(json_object(
+					'id', id,
+					'content', content,
+					'thread', thread,
+					'fractional_index', fractional_index
+				) ORDER BY fractional_index ASC, id ASC)
+				FROM thread_cards
+				WHERE thread_cards.thread = t4.id
 			)
 		) AS json,
 		fractional_index,
@@ -79,6 +124,16 @@ d3 AS (
 				SELECT json_group_array(json(json))
 				FROM d4
 				WHERE d4.parent_thread = t3.id
+			),
+			'cards', (
+				SELECT json_group_array(json_object(
+					'id', id,
+					'content', content,
+					'thread', thread,
+					'fractional_index', fractional_index
+				) ORDER BY fractional_index ASC, id ASC)
+				FROM thread_cards
+				WHERE thread_cards.thread = t3.id
 			)
 		) AS json,
 		fractional_index,
@@ -97,7 +152,17 @@ SELECT
 			SELECT json_group_array(json(json))
 			FROM d3
 			WHERE d3.parent_thread = t2.id
-		)
+		),
+		'cards', (
+      SELECT json_group_array(json_object(
+        'id', id,
+        'content', content,
+				'thread', thread,
+				'fractional_index', fractional_index
+      ) ORDER BY fractional_index ASC, id ASC)
+      FROM thread_cards
+      WHERE thread_cards.thread = t2.id
+    )
 		) AS json,
 		fractional_index,
 		parent_thread
@@ -115,7 +180,17 @@ SELECT
 			SELECT json_group_array(json(json))
 			FROM d2
 			WHERE d2.parent_thread = t1.id
-		)
+		),
+		'cards', (
+      SELECT json_group_array(json_object(
+        'id', id,
+        'content', content,
+				'thread', thread,
+				'fractional_index', fractional_index
+      ) ORDER BY fractional_index ASC, id ASC)
+      FROM thread_cards
+      WHERE thread_cards.thread = t1.id
+    )
 	) AS json,
 	fractional_index,
 	parent_thread
@@ -133,7 +208,17 @@ SELECT
 			SELECT json_group_array(json(json))
 			FROM d1
 			WHERE d1.parent_thread = t0.id
-		)
+		),
+		'cards', (
+      SELECT json_group_array(json_object(
+        'id', id,
+        'content', content,
+				'thread', thread,
+				'fractional_index', fractional_index
+      ) ORDER BY fractional_index ASC, id ASC)
+      FROM thread_cards
+      WHERE thread_cards.thread = t0.id
+    )
 	) AS json
 	FROM thread_tree AS t0
 	WHERE depth = 0
--- src/lib/Models/threadTree.test.ts
+++ src/lib/Models/threadTree.test.ts
@@ -11,20 +11,60 @@ describe("get threadtree", async () => {
   });
   const result = await injectedGetLiveThreadTree(id);
   const tree = result._unsafeUnwrap({ withStackTrace: true });
-  test("fractional index ordering", () => {
-    if (!tree.child_threads) throw new Error("test failed");
-    expect(tree.child_threads[0]?.title).toBe("1");
-    expect(tree.child_threads[1]?.title).toBe("2");
-    expect(tree.child_threads[2]?.title).toBe("3");
-    expect(tree.child_threads[3]?.title).toBe("4");
-    expect(tree.child_threads[4]?.title).toBe("5");
+
+  describe("thread", () => {
+    test("fractional index ordering", () => {
+      if (!tree.child_threads) throw new Error("test failed");
+      expect(tree.child_threads[0]?.title).toBe("1");
+      expect(tree.child_threads[1]?.title).toBe("2");
+      expect(tree.child_threads[2]?.title).toBe("3");
+      expect(tree.child_threads[3]?.title).toBe("4");
+      expect(tree.child_threads[4]?.title).toBe("5");
+    });
+
+    test("exclude deleted thread", () => {
+      if (!tree.child_threads) throw new Error("test failed");
+      expect(tree.child_threads[5]).toBeUndefined();
+    });
+
+    test("tree structuring", () => {
+      expect(
+        // @ts-expect-error thread nesting test
+        tree.child_threads[0].child_threads[0].child_threads[0].child_threads[0]
+          .child_threads[0].child_threads[0].id,
+      ).toBeTruthy();
+    });
   });
 
-  test("tree structuring", () => {
-    expect(
-      tree.child_threads[0].child_threads[0].child_threads[0].child_threads[0]
-        .child_threads[0].child_threads[0].id,
-    ).toBeTruthy();
+  describe("card", () => {
+    test("card creation", async () => {
+      const result = await ELECTRIC_TEST.db.cards.findMany({
+        where: { thread: id },
+      });
+      expect(result.length).toBe(6);
+    });
+
+    test("fractional index ordering", () => {
+      if (!tree.cards) throw new Error("test failed");
+      expect(tree.cards[0]?.content).toBe("1");
+      expect(tree.cards[1]?.content).toBe("2");
+      expect(tree.cards[2]?.content).toBe("3");
+      expect(tree.cards[3]?.content).toBe("4");
+      expect(tree.cards[4]?.content).toBe("5");
+    });
+
+    test("exclude deleted thread", () => {
+      if (!tree.child_threads) throw new Error("test failed");
+      expect(tree.cards[5]).toBeUndefined();
+    });
+
+    test("tree structuring", () => {
+      expect(
+        // @ts-expect-error thread nesting test
+        tree.child_threads[0].child_threads[0].child_threads[0].child_threads[0]
+          .child_threads[0].child_threads[0]?.cards[0].id,
+      ).toBeTruthy();
+    });
   });
 });
 
@@ -114,5 +154,74 @@ async function createTree(depth: number, parent_thread?: string) {
     await createTree(depth + 1, currentID);
   }
 
+  await createCard(currentID);
+
   return currentID;
+}
+
+async function createCard(thread: string) {
+  const first = generateKeyBetween(null, null);
+  const third = generateKeyBetween(first, null);
+  const second = generateKeyBetween(first, third);
+  const fourth = third;
+  const fifth = third;
+  const now = new Date();
+  await ELECTRIC_TEST.db.cards.createMany({
+    data: [
+      {
+        id: thread,
+        content: "1",
+        thread: thread,
+        fractional_index: first,
+        deleted: false,
+        created_at: now,
+        updated_at: now,
+      },
+      {
+        id: uuidv7(),
+        content: "2",
+        thread: thread,
+        fractional_index: second,
+        deleted: false,
+        created_at: now,
+        updated_at: now,
+      },
+      {
+        id: uuidv7(),
+        content: "3",
+        thread: thread,
+        fractional_index: third,
+        deleted: false,
+        created_at: now,
+        updated_at: now,
+      },
+      {
+        id: uuidv7(),
+        content: "4",
+        thread: thread,
+        fractional_index: fourth,
+        deleted: false,
+        created_at: now,
+        updated_at: now,
+      },
+      {
+        id: uuidv7(),
+        content: "5",
+        thread: thread,
+        fractional_index: fifth,
+        deleted: false,
+        created_at: now,
+        updated_at: now,
+      },
+      {
+        id: uuidv7(),
+        content: "deleted",
+        thread: thread,
+        fractional_index: fifth,
+        deleted: true,
+        created_at: now,
+        updated_at: now,
+      },
+    ],
+  });
 }
