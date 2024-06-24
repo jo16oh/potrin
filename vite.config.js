363eb3420a41e44a7418e03bbabfb9253098651f
Hajime Nagisa <jyoichirou99@gmail.com>
2024-06-24 23:25:04 +0900

    chore: exclude generated folder from vitest watch

--- vite.config.js
+++ vite.config.js
@@ -35,6 +35,7 @@ export default defineConfig(async () => ({
   test: {
     globalSetup: "./vitest.setup.ts",
     include: ["src/**/*.{test,test.svelte,spec}.{js,ts}"],
+    exclude: ["src/generated/**"],
     environment: "jsdom",
   },
 }));
