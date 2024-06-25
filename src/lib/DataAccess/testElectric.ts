aad2abf81cc267bef6fb6f8f6310b6e6f5f3185b
Hajime Nagisa <jyoichirou99@gmail.com>
2024-06-25 17:14:05 +0900

    test: cleanup function for testElectricSync

--- src/lib/DataAccess/testElectric.ts
+++ src/lib/DataAccess/testElectric.ts
@@ -1,4 +1,4 @@
-import { test, onTestFinished } from "vitest";
+import { test, onTestFinished, afterEach } from "vitest";
 import Database from "better-sqlite3";
 import type { Database as SQLite } from "better-sqlite3";
 import type { ElectricClient } from "electric-sql/client/model";
@@ -40,11 +40,18 @@ export const testElectricSync = test.extend<TestElectricSync>({
     const sqlite = new Database(":memory:");
     const e1 = await createElectric(sqlite);
     await use(e1);
+    afterEach(async () => {
+      await cleanup(e1);
+    });
   },
+
   e2: async ({}, use) => {
     const sqlite = new Database("");
     const e2 = await createElectric(sqlite);
     await use(e2);
+    afterEach(async () => {
+      await cleanup(e2);
+    });
   },
   token: token,
 });
@@ -59,4 +66,13 @@ async function createElectric(sqlite: SQLite) {
   });
 
   return electric;
+}
+
+async function cleanup(e: ElectricClient<typeof schema>) {
+  await e.db.threads.deleteMany();
+  await e.db.cards.deleteMany();
+  const threads = await e.db.threads.sync();
+  const cards = await e.db.cards.sync();
+  await threads.synced;
+  await cards.synced;
 }
