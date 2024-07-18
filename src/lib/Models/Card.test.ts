053827e29e3813e32ecf70072dddf93d276f4125
Hajime Nagisa <jyoichirou99@gmail.com>
2024-07-18 12:27:03 +0900

    refactor: create CardYDoc model
    - call storePendingUpdates before checkpoint
    - insert record to card_checkpoints table

--- src/lib/Models/Card.test.ts
+++ src/lib/Models/Card.test.ts
@@ -3,8 +3,6 @@ import { Card } from "$lib/Models/Card";
 import { testElectric } from "$lib/DataAccess/testElectric";
 import { Thread } from "./Thread";
 import { uuidv7 } from "uuidv7";
-import * as Y from "yjs";
-import { YDocMatelializer } from "$lib/DataAccess/YDocMaterializer";
 
 describe("card", () => {
   testElectric("create card", async ({ electric }) => {
@@ -63,78 +61,5 @@ describe("card", () => {
   testElectric("fails if the thread doesn't exists", async ({ electric }) => {
     const injectedCreateCard = Card.create.inject({ ELECTRIC: electric });
     expect(injectedCreateCard({ thread_id: uuidv7() })).rejects.toThrow();
-  });
-
-  testElectric("getElectrcYDoc", async ({ electric }) => {
-    const injectedCreateThread = Thread.create.inject({ ELECTRIC: electric });
-    const injectedCreateCard = Card.create.inject({ ELECTRIC: electric });
-    const injectedGetCardYDoc = Card.getElectricYDoc.inject({
-      ELECTRIC: electric,
-    });
-
-    const thread = await injectedCreateThread();
-    const card = await injectedCreateCard({ thread_id: thread.id });
-    const ydoc = await injectedGetCardYDoc(card.id);
-    const xml = ydoc.getXmlFragment("prosemirror");
-
-    xml.insert(0, [new Y.XmlElement("div")]);
-
-    await new Promise((resolve) => setTimeout(resolve, 100));
-    const materialized = await electric.db.cards.findUnique({
-      where: { id: card.id },
-    });
-
-    expect(materialized.content).toBe(xml.toString());
-  });
-
-  testElectric("checkpoint", async ({ electric }) => {
-    const injectedCreateThread = Thread.create.inject({ ELECTRIC: electric });
-    const injectedCreateCard = Card.create.inject({ ELECTRIC: electric });
-    const injectedGetCardYDoc = Card.getElectricYDoc.inject({
-      ELECTRIC: electric,
-    });
-    const injectedCheckpoint = Card.checkpoint.inject({
-      ELECTRIC: electric,
-    });
-
-    const thread = await injectedCreateThread();
-    const card = await injectedCreateCard({ thread_id: thread.id });
-
-    {
-      const ydoc = await injectedGetCardYDoc(card.id);
-      const xml = ydoc.getXmlFragment("prosemirror");
-      xml.insert(0, [new Y.XmlElement("div")]);
-      xml.insert(0, [new Y.XmlElement("div")]);
-      await injectedCheckpoint(card.id);
-      xml.insert(0, [new Y.XmlElement("div")]);
-      xml.insert(0, [new Y.XmlElement("div")]);
-      await injectedCheckpoint(card.id);
-    }
-
-    {
-      const updates = await electric.db.card_ydoc_updates.findMany();
-      expect(updates.length).toBe(4);
-    }
-
-    {
-      const checkpoints = await electric.db.card_ydoc_updates.findMany({
-        where: { checkpoint: true },
-        orderBy: { created_at: "desc" },
-      });
-      expect(checkpoints.length).toBe(2);
-    }
-
-    {
-      await YDocMatelializer.mergeCardUpdates(card.id, 1000, electric);
-      const updates = await electric.db.card_ydoc_updates.findMany();
-      expect(updates.length).toBe(2);
-
-      const ydoc = await injectedGetCardYDoc(card.id);
-      const xml = ydoc.getXmlFragment("prosemirror");
-
-      expect(xml.toString()).toBe(
-        "<div></div><div></div><div></div><div></div>",
-      );
-    }
   });
 });
