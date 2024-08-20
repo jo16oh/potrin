f280a96eabe1b698bcdaafa652357c7d6c152039
Joichiro Hayashi <jyoichirou99@gmail.com>
2024-08-20 13:50:28 +0900

    exclude "src/generated" from eslint target

--- eslint.config.js
+++ eslint.config.js
@@ -5,17 +5,18 @@ import eslintConfigPrettier from "eslint-config-prettier";
 import eslintPluginSvelte from "eslint-plugin-svelte";
 import globals from "globals";
 
-export default [
-  eslint.configs.recommended,
-  eslintConfigPrettier,
-  ...tseslint.configs.strict,
-  ...eslintPluginSvelte.configs["flat/prettier"],
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-      },
+export default tseslint.config({
+  ignores: ["src/generated/**"],
+  languageOptions: {
+    globals: {
+      ...globals.browser,
+      ...globals.node,
     },
   },
-];
+  extends: [
+    eslint.configs.recommended,
+    eslintConfigPrettier,
+    ...tseslint.configs.strict,
+    ...eslintPluginSvelte.configs["flat/prettier"],
+  ],
+});
