ff2947563a15f8e3450721bd3212b8d1c33e29fb
Joichiro Hayashi <jyoichirou99@gmail.com>
2024-08-20 12:27:40 +0900

    chore: remove old electric-sql and all its related codes

--- vite.config.js
+++ vite.config.js
@@ -37,9 +37,5 @@ export default defineConfig(async () => ({
     include: ["src/**/*.{test,test.svelte,spec}.{js,ts}"],
     exclude: ["src/generated/**"],
     environment: "jsdom",
-    poolOptions: {
-      threads: { singleThread: true },
-      forks: { singleFork: true },
-    },
   },
 }));
