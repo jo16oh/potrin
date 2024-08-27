d02b91a0c86a1c4ad3115ec07ae10097fdd83bf1
Joichiro Hayashi <jyoichirou99@gmail.com>
2024-08-27 20:40:28 +0900

    remove tauri-plugin-store

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,7 +4,6 @@
 fn main() {
     potrin_lib::run();
     tauri::Builder::default()
-        .plugin(tauri_plugin_store::Builder::new().build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
