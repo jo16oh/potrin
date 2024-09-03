56d96ebcead0b49a4cb868e3ba803805fee09b65
Joichiro Hayashi <jyoichirou99@gmail.com>
2024-09-04 00:41:10 +0900

    remove comment-outed code

--- src-tauri/db/migrations/20240828100046_oplog.sql
+++ src-tauri/db/migrations/20240828100046_oplog.sql
@@ -8,20 +8,6 @@ CREATE TABLE oplog (
   status BLOB
 ) STRICT;
 
--- CREATE TRIGGER after_update_oplog$delete_when_synced_and_indexed
--- AFTER UPDATE ON oplog
--- FOR EACH ROW
--- WHEN
---   NEW.synced = 1 AND NEW.indexed = 1
--- BEGIN
---   DELETE FROM oplog
---   WHERE
---     primary_key = NEW.primary_key
---     AND updated_at <= NEW.updated_at
---     AND counter <= NEW.counter
---   ;
--- END;
-
 CREATE TRIGGER after_update_oplog$update_counter
 AFTER UPDATE ON oplog
 FOR EACH ROW
