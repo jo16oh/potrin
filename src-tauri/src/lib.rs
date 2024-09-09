9fc3302fadf5a965867340beb0f4193411a44b8a
Joichiro Hayashi <jyoichirou99@gmail.com>
2024-09-09 17:40:33 +0900

    print panic info in test runtime

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -142,12 +142,31 @@ pub mod test {
     #[macro_export]
     macro_rules! run_in_mock_app {
             (|$arg:ident: $arg_type:ty| async $closure:block) => {{
+                let is_successful = Arc::new(AtomicBool::new(false));
 
-                panic::set_hook(Box::new(|_| {}));
+                let flag_clone = Arc::clone(&is_successful);
+                panic::set_hook(Box::new(move |panic_info| {
+                    if !flag_clone.load(SeqCst) {
+                        if let Some(location) = panic_info.location() {
+                            println!("panic occurred in file '{}' at line {}",
+                                location.file(),
+                                location.line(),
+                            );
+                        } else {
+                            println!("panic occurred but can't get location information...");
+                        }
 
-                let is_successfull = Arc::new(AtomicBool::new(false));
+                        if let Some(s) = panic_info.payload().downcast_ref::<&str>() {
+                            println!("{s:?}");
+                        } else if let Some(s) = panic_info.payload().downcast_ref::<String>() {
+                            println!("{s:?}");
+                        } else {
+                            println!("panic occurred");
+                        }
+                    };
+                }));
 
-                let flag_clone = is_successfull.clone();
+                let flag_clone = is_successful.clone();
                 let handle = thread::spawn(|| {
                     println!("thread running");
                     std::panic::catch_unwind(|| {
@@ -164,7 +183,7 @@ pub mod test {
 
                 let _ = handle.join();
 
-                assert!(is_successfull.load(SeqCst));
+                assert!(is_successful.load(SeqCst));
             }};
         }
 }
