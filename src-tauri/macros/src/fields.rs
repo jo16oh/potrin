51cfc55c60e1e1325ebf54e192080944c0e3d234
Joichiro Hayashi <jyoichirou99@gmail.com>
2024-09-27 16:19:02 +0900

    fix: remove serde rename
    polodb renames fields when serializing structs into bson, but the
    opposite not works

--- src-tauri/macros/src/fields.rs
+++ src-tauri/macros/src/fields.rs
@@ -1,4 +1,4 @@
-use heck::{ToLowerCamelCase, ToPascalCase};
+use heck::ToPascalCase;
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
 use syn::{parse_macro_input, Ident, ItemStruct};
@@ -10,6 +10,10 @@ pub fn fields_impl(input: TokenStream) -> TokenStream {
     let fields = &input.fields;
 
     let field_names = fields.iter().map(|f| &f.ident);
+    let field_names_string: Vec<String> = fields
+        .iter()
+        .map(|f| f.ident.as_ref().unwrap().to_string())
+        .collect();
 
     let pascal_case_field_names: Vec<Ident> = fields
         .iter()
@@ -20,21 +24,12 @@ pub fn fields_impl(input: TokenStream) -> TokenStream {
         })
         .collect();
 
-    let camel_case_field_names: Vec<String> = fields
-        .iter()
-        .map(|f| {
-            let ident = f.ident.as_ref().unwrap();
-            ident.to_string().to_lower_camel_case()
-        })
-        .collect();
-
     let field_types = fields.iter().map(|f| &f.ty);
 
     quote! {
         #input
 
         #[derive(serde::Serialize, serde::Deserialize, Debug, Clone, specta::Type)]
-        #[serde(rename_all = "camelCase")]
         pub enum #enum_name {
            #(#pascal_case_field_names(#field_types)),*
         }
@@ -42,7 +37,7 @@ pub fn fields_impl(input: TokenStream) -> TokenStream {
         impl Into<polodb_core::bson::Bson> for #enum_name {
             fn into(self) -> polodb_core::bson::Bson {
                 match self {
-                    #(#enum_name::#pascal_case_field_names(value) => polodb_core::bson::bson!({ #camel_case_field_names: value })),*
+                    #(#enum_name::#pascal_case_field_names(value) => polodb_core::bson::bson!({ #field_names_string: value })),*
                 }
             }
         }
--- src-tauri/src/state/types.rs
+++ src-tauri/src/state/types.rs
@@ -3,7 +3,6 @@ use macros::Bson;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, specta::Type)]
-#[serde(rename_all = "camelCase")]
 pub struct AppState {
     pub client: ClientState,
     pub user: Option<UserState>,
@@ -13,14 +12,12 @@ pub struct AppState {
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, Bson, specta::Type)]
-#[serde(rename_all = "camelCase")]
 #[macros::fields]
 pub struct ClientState {
     pub id: Base64String,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, Bson, specta::Type)]
-#[serde(rename_all = "camelCase")]
 #[macros::fields]
 pub struct UserState {
     pub id: Base64String,
@@ -28,29 +25,25 @@ pub struct UserState {
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, Bson, specta::Type)]
-#[serde(rename_all = "camelCase")]
 #[macros::fields]
 pub struct PotState {
     pub id: Base64String,
     pub sync: bool,
 }
 
-#[derive(Serialize, Deserialize, Clone, Bson, specta::Type)]
-#[serde(rename_all = "camelCase")]
+#[derive(Serialize, Deserialize, Debug, Clone, Bson, specta::Type)]
 #[macros::fields]
 pub struct WorkspaceState {
     pub tabs: Vec<TabState>,
     pub focused_tab_idx: Option<i64>,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, Bson, specta::Type)]
-#[serde(rename_all = "camelCase")]
+#[derive(Serialize, Deserialize, Debug, Clone, Bson, specta::Type)]
 pub struct TabState {
     pub id: Base64String,
     pub view: String,
     pub scroll_pos: i64,
 }
 
-#[derive(Serialize, Deserialize, Clone, Bson, specta::Type)]
-#[serde(rename_all = "camelCase")]
+#[derive(Serialize, Deserialize, Debug, Clone, Bson, specta::Type)]
 pub struct SettingState {}
