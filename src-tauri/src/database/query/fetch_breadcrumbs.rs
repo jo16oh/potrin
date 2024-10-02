ef33b42d90f56384a2ab63e7353efb3354913fc4
Joichiro Hayashi <jyoichirou99@gmail.com>
2024-10-02 12:08:38 +0900

    fix: fix variable lengh args in fetch_breadcrumbs

--- src-tauri/src/database/query/fetch_breadcrumbs.rs
+++ src-tauri/src/database/query/fetch_breadcrumbs.rs
@@ -15,32 +15,38 @@ pub async fn fetch_breadcrumbs(
     parent_ids: Vec<&Base64String>,
     pool: &SqlitePool,
 ) -> anyhow::Result<Vec<Breadcrumb>> {
-    let id_string = parent_ids
-        .iter()
-        .map(|id| id.to_string())
-        .collect::<Vec<String>>()
-        .join(", ");
-
-    query_as!(
-        Breadcrumb,
+    let query = format!(
         r#"
             WITH RECURSIVE breadcrumbs AS (
                 SELECT
                     id, parent_id, text
                 FROM outlines
-                WHERE id IN (?) AND is_deleted = false
+                WHERE id IN ({}) AND is_deleted = false
                 UNION ALL
                 SELECT
-                    child.id, child.parent_id, child.text
-                FROM breadcrumbs AS parent
-                JOIN outlines AS child ON parent.id = child.parent_id
-                WHERE child.is_deleted = false
+                    parent.id, parent.parent_id, parent.text
+                FROM breadcrumbs AS child
+                JOIN outlines AS parent ON parent.id = child.parent_id
+                WHERE parent.is_deleted = false
             )
             SELECT id, parent_id, text FROM breadcrumbs;
-            "#,
-        id_string
-    )
-    .fetch_all(pool)
-    .await
-    .map_err(|e| anyhow::anyhow!(e.to_string()))
+        "#,
+        parent_ids
+            .iter()
+            .map(|_| "?".to_string())
+            .collect::<Vec<String>>()
+            .join(", ")
+    );
+
+    let mut query_builder = query_as::<_, Breadcrumb>(&query);
+
+    for id in parent_ids {
+        query_builder = query_builder.bind(id);
+    }
+
+    query_builder
+        .fetch_all(pool)
+        .await
+        .map_err(|e| anyhow::anyhow!(e.to_string()))
 }
+
