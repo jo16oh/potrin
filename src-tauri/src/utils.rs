53f043b699df085dfc72d0a6698372830803586a
Joichiro Hayashi <jyoichirou99@gmail.com>
2024-10-04 14:47:00 +0900

    fix: remove redundant code

--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -1,15 +1,11 @@
-use std::sync::OnceLock;
 use anyhow::anyhow;
+use std::sync::OnceLock;
 
 pub fn set_once_lock<T>(lock: &OnceLock<T>, value: T) -> anyhow::Result<()> {
     lock.set(value)
-        .map_err(|_| anyhow!("Failed to set value to OnceLock"))?;
-    Ok(())
+        .map_err(|_| anyhow!("Failed to set value to OnceLock"))
 }
 
 pub fn get_once_lock<T>(lock: &OnceLock<T>) -> anyhow::Result<&T> {
-    let result = lock
-        .get()
-        .ok_or_else(|| anyhow!("Failed to get value of OnceLock"))?;
-    Ok(result)
+    lock.get().ok_or(anyhow!("Failed to get value of OnceLock"))
 }
