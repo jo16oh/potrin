ee3df688799ea7b941a99480765f24254efc09ac
Joichiro Hayashi <jyoichirou99@gmail.com>
2024-10-04 13:23:11 +0900

    fix: relation tables definition

--- src-tauri/db/migrations/20241001083313_relations.sql
+++ src-tauri/db/migrations/20241001083313_relations.sql
@@ -1,6 +1,6 @@
 CREATE TABLE outline_links(
-    id_from BLOB REFERENCES outline(id),
-    id_to BLOB REFERENCES outline(id),
+    id_from BLOB REFERENCES outlines(id) ON DELETE CASCADE,
+    id_to BLOB REFERENCES outlines(id) ON DELETE CASCADE,
     PRIMARY KEY (id_from, id_to)
 ) STRICT;
 
@@ -8,8 +8,8 @@ CREATE INDEX outline_links$from_idx ON outline_links(id_from);
 CREATE INDEX outline_links$to_idx ON outline_links(id_to);
 
 CREATE TABLE card_links (
-    id_from BLOB REFERENCES card(id),
-    id_to BLOB REFERENCES outline(id),
+    id_from BLOB REFERENCES cards(id) ON DELETE CASCADE,
+    id_to BLOB REFERENCES outlines(id) ON DELETE CASCADE,
     PRIMARY KEY (id_from, id_to)
 ) STRICT;
 
@@ -17,8 +17,8 @@ CREATE INDEX card_links$from_idx ON card_links(id_from);
 CREATE INDEX card_links$to_idx ON card_links(id_to);
 
 CREATE TABLE card_quotes (
-    id_from BLOB REFERENCES card(id),
-    id_to BLOB REFERENCES card(id),
+    id_from BLOB REFERENCES cards(id) ON DELETE CASCADE,
+    id_to BLOB REFERENCES cards(id) ON DELETE CASCADE,
     PRIMARY KEY (id_from, id_to)
 ) STRICT;
 
