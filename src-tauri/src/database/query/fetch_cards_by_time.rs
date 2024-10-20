e4b8dc07d03787edb5c322bf0ff2b90418940741
Joichiro Hayashi <jyoichirou99@gmail.com>
2024-10-20 16:39:16 +0900

    fix: fetch quoted card together when fetch_cards_by_time

--- src-tauri/src/database/query/fetch_cards_by_time.rs
+++ src-tauri/src/database/query/fetch_cards_by_time.rs
@@ -14,15 +14,34 @@ pub async fn fetch_cards_by_created_at(
     sqlx::query_as!(
         RawCard,
         r#"
-            SELECT
-                cards.id, cards.outline_id, cards.fractional_index, cards.text,
-                cards.version_id AS version_id,
-                quotes.quoted_card_id AS quoted_card_id,
-                quotes.version_id AS quote_version_id
-            FROM cards
-            LEFT JOIN quotes ON cards.id = quotes.card_id
-            WHERE ? <= created_at AND created_at < ? AND is_deleted = false
-            ORDER BY created_at DESC;
+            WITH c1 AS (
+                SELECT
+                    cards.id, cards.outline_id, cards.fractional_index, cards.text,
+                    cards.version_id AS version_id,
+                    quotes.quoted_card_id AS quoted_card_id,
+                    quotes.version_id AS quote_version_id,
+                    cards.created_at AS created_at
+                FROM cards
+                LEFT JOIN quotes ON cards.id = quotes.card_id
+                WHERE ? <= created_at AND created_at < ? AND is_deleted = false
+                UNION 
+                SELECT
+                    cards.id, cards.outline_id, cards.fractional_index, cards.text,
+                    cards.version_id AS version_id,
+                    quotes.quoted_card_id AS quoted_card_id,
+                    quotes.version_id AS quote_version_id,
+                    cards.created_at AS created_at
+                FROM cards
+                LEFT JOIN quotes ON cards.id = quotes.card_id
+                INNER JOIN c1 ON cards.id = c1.quoted_card_id
+                WHERE is_deleted = false
+                ORDER BY created_at DESC
+                
+            )
+            SELECT 
+                id, outline_id, fractional_index, text, version_id, 
+                quoted_card_id, quote_version_id 
+            FROM c1;
         "#,
         from,
         to,
@@ -32,64 +51,3 @@ pub async fn fetch_cards_by_created_at(
     .map(|raw_cards| raw_cards.into_iter().map(Card::from).collect())
     .map_err(|e| anyhow!(e))
 }
-
-// pub async fn fetch_cards_by_updated_at(
-//     pool: &SqlitePool,
-//     from: DateTime<Utc>,
-//     to: DateTime<Utc>,
-// ) -> anyhow::Result<Vec<Card>> {
-//     let from = from.timestamp_millis();
-//     let to = to.timestamp_millis();
-//     sqlx::query_as!(
-//         RawCard,
-//         r#"
-//             SELECT
-//                 cards.id, cards.outline_id, cards.fractional_index, cards.text,
-//                 cards.version_id AS version_id,
-//                 quotes.quoted_card_id AS quoted_card_id,
-//                 quotes.version_id AS quote_version_id
-//             FROM cards
-//             LEFT JOIN quotes ON cards.id = quotes.card_id
-//             WHERE ? <= cards.updated_at AND cards.updated_at < ? AND is_deleted = false
-//             ORDER BY updated_at DESC;
-//         "#,
-//         from,
-//         to,
-//     )
-//     .fetch_all(pool)
-//     .await
-//     .map(|raw_cards| raw_cards.into_iter().map(Card::from).collect())
-//     .map_err(|e| anyhow!(e))
-// }
-
-// pub async fn fetch_cards_by_created_at_and_updated_at(
-//     pool: &SqlitePool,
-//     from: DateTime<Utc>,
-//     to: DateTime<Utc>,
-// ) -> anyhow::Result<Vec<Card>> {
-//     let from = from.timestamp_millis();
-//     let to = to.timestamp_millis();
-//     sqlx::query_as!(
-//         RawCard,
-//         r#"
-//             SELECT
-//                 cards.id, cards.outline_id, cards.fractional_index, cards.text,
-//                 cards.version_id AS version_id,
-//                 quotes.quoted_card_id AS quoted_card_id,
-//                 quotes.version_id AS quote_version_id
-//             FROM cards
-//             LEFT JOIN quotes ON cards.id = quotes.card_id
-//             WHERE
-//                 ((? <= updated_at AND updated_at < ?) OR (? <= created_at AND created_at < ?))
-//                 AND is_deleted = false;
-//         "#,
-//         from,
-//         to,
-//         from,
-//         to
-//     )
-//     .fetch_all(pool)
-//     .await
-//     .map(|raw_cards| raw_cards.into_iter().map(Card::from).collect())
-//     .map_err(|e| anyhow!(e))
-// }
