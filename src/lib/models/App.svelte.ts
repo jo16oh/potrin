a6ef6eba96f96bc032f52c5c3a78636527764760
Joichiro Hayashi <jyoichirou99@gmail.com>
2025-01-22 21:16:30 +0900

    refactor: simplify state handling

--- src/lib/models/App.svelte.ts
+++ src/lib/models/App.svelte.ts
@@ -3,10 +3,8 @@ import {
   commands as tauriCommands,
   events,
   type AppState,
-  type UserState,
 } from "../../generated/tauri-commands";
-import { applyPatch, compare } from "fast-json-patch";
-import { deepCloneOwnProperties } from "$lib/utils";
+import { applyPatch, observe } from "fast-json-patch";
 
 const key = Symbol();
 
@@ -16,129 +14,33 @@ type Commands = Pick<
 >;
 let commands: Commands = tauriCommands;
 
-declare const AppBrand: unique symbol;
-
-type AppAccessor = AppState & {
-  openPot: (pot: { id: string; name: string }) => void;
-};
-
-export type App = AppAccessor & { [AppBrand]: unknown };
-
 export const App = {
   inject(cmds: Commands) {
     commands = cmds;
   },
   init(value: AppState) {
-    const [app, setApp] = App.new(value);
+    let state = $state(value);
+    let prev = $state.snapshot(state);
 
-    let prev: AppState | undefined;
-
-    $effect(() => {
-      const current = deepCloneOwnProperties(app);
-      if (prev) {
-        const diff = compare(prev, current);
-        if (diff.length !== 0) commands.updateAppState(JSON.stringify(diff));
-      }
-      prev = current;
+    observe(state, (patches) => {
+      commands
+        .updateAppState(JSON.stringify(patches))
+        .then(() => {
+          prev = $state.snapshot(state);
+        })
+        .catch((e) => {
+          state = prev;
+          console.error(e);
+        });
     });
 
     events.appStateChange.listen((e) => {
-      applyPatch(prev, JSON.parse(e.payload.patch));
-      setApp(prev!);
+      applyPatch(state, JSON.parse(e.payload.patch));
     });
 
-    setContext(key, app);
+    setContext(key, state);
   },
   state() {
-    return getContext<App>(key);
-  },
-  new(value: AppState): [App, (v: AppState) => void] {
-    let [user, setUser] = value.user ? User.new(value.user) : [null, null];
-
-    let app = $state({
-      clientId: value.clientId,
-      user: user,
-      pots: value.pots,
-      setting: value.setting,
-    });
-
-    const accessor: AppAccessor = {
-      get clientId() {
-        return app.clientId;
-      },
-      get user() {
-        return app.user;
-      },
-      get pots() {
-        return app.pots;
-      },
-      get setting() {
-        return app.setting;
-      },
-      set user(u: UserState | null) {
-        if (u && setUser) {
-          setUser(u);
-          app.user = user;
-        } else if (u) {
-          [user, setUser] = User.new(u);
-          app.user = user;
-        } else {
-          app.user = null;
-        }
-      },
-      openPot(pot: { id: string; name: string }) {
-        app.pots[pot.id] = pot.name;
-        commands.openPot(pot.id, pot.name);
-      },
-    };
-
-    const setApp = (value: AppState) => {
-      app = {
-        clientId: value.clientId,
-        user: value.user ? User.new(value.user)[0] : null,
-        pots: value.pots,
-        setting: value.setting,
-      };
-    };
-
-    return [accessor as App, setApp];
-  },
-};
-
-declare const UserBrand: unique symbol;
-
-type UserAccessor = UserState;
-
-export type User = UserAccessor & { [UserBrand]: unknown };
-
-const User = {
-  new(value: UserState): [User, (u: UserState) => void] {
-    // validation
-    let user = $state({
-      id: value.id,
-      name: value.name,
-    });
-
-    const accessor: UserAccessor = {
-      get id() {
-        return user.id;
-      },
-      get name() {
-        return user.name;
-      },
-      set name(n) {
-        user.name = n;
-      },
-    };
-
-    const setUser = (value: UserState) => {
-      // validation
-      user = {
-        id: value.id,
-        name: value.name,
-      };
-    };
-
-    return [accessor as User, setUser];
+    return getContext<AppState>(key);
   },
 };
--- src/lib/models/Workspace.svelte.ts
+++ src/lib/models/Workspace.svelte.ts
@@ -1,16 +1,10 @@
-import { applyPatch, compare } from "fast-json-patch";
+import { getContext, setContext } from "svelte";
 import {
   events,
   commands as tauriCommands,
-  type FocusState,
-  type PotState,
-  type SidebarState,
-  type TabState,
-  type ViewState,
   type WorkspaceState,
 } from "../../generated/tauri-commands";
-import { getContext, setContext } from "svelte";
-import { deepCloneOwnProperties } from "$lib/utils";
+import { applyPatch, observe } from "fast-json-patch";
 
 const key = Symbol();
 
@@ -20,250 +14,33 @@ type Commands = Pick<
 >;
 let commands: Commands = tauriCommands;
 
-declare const WorkspaceBrand: unique symbol;
-
-type WorkspaceAccessor = {
-  pot: Pot;
-  tabs: Tab[];
-  focus: FocusState;
-  sidebar: Sidebar;
-} & {
-  focusTo: (to: "timeline" | "search" | number) => void;
-};
-
-export type Workspace = WorkspaceAccessor & {
-  [WorkspaceBrand]: unknown;
-};
-
 export const Workspace = {
   inject(cmds: Commands) {
     commands = cmds;
   },
   init(value: WorkspaceState) {
-    const [workspace, setWorkspace] = Workspace.new(value);
+    let state = $state(value);
+    let prev = $state.snapshot(state);
 
-    let prev: WorkspaceState | undefined;
-
-    $effect(() => {
-      const current = deepCloneOwnProperties(workspace);
-      if (prev) {
-        const diff = compare(prev, current);
-        if (diff.length !== 0)
-          commands.updateWorkspaceState(JSON.stringify(diff));
-      }
-      prev = current;
+    observe(state, (patches) => {
+      commands
+        .updateWorkspaceState(JSON.stringify(patches))
+        .then(() => {
+          prev = $state.snapshot(state);
+        })
+        .catch((e) => {
+          state = prev;
+          console.error(e);
+        });
     });
 
     events.workspaceStateChange.listen((e) => {
-      applyPatch(prev, JSON.parse(e.payload.patch));
-      setWorkspace(prev!);
+      applyPatch(state, JSON.parse(e.payload.patch));
     });
 
-    setContext(key, workspace);
+    setContext(key, state);
   },
   state() {
-    return getContext<Workspace>(key);
-  },
-  new(value: WorkspaceState): [Workspace, (value: WorkspaceState) => void] {
-    let workspace = $state({
-      pot: Pot.new(value.pot)[0],
-      tabs: value.tabs.map((t) => Tab.new(t)[0]),
-      focus: value.focus,
-      sidebar: Sidebar.new(value.sidebar)[0],
-    });
-
-    const accessor: WorkspaceAccessor = {
-      get pot() {
-        return workspace.pot;
-      },
-      get tabs() {
-        return workspace.tabs;
-      },
-      get focus() {
-        return workspace.focus;
-      },
-      get sidebar() {
-        return workspace.sidebar;
-      },
-      focusTo(to) {
-        if (to === "timeline") {
-          workspace.focus = {
-            timeline: {},
-          };
-        } else if (to === "search") {
-          workspace.focus = {
-            search: {},
-          };
-        } else {
-          workspace.focus = {
-            tabs: { index: to },
-          };
-        }
-      },
-    };
-
-    const setWorkspace = (value: WorkspaceState) => {
-      workspace = {
-        pot: Pot.new(value.pot)[0],
-        tabs: value.tabs.map((t) => Tab.new(t)[0]),
-        focus: value.focus,
-        sidebar: Sidebar.new(value.sidebar)[0],
-      };
-    };
-
-    return [accessor as Workspace, setWorkspace];
-  },
-};
-
-declare const PotBrand: unique symbol;
-
-type PotAccessor = PotState;
-export type Pot = PotAccessor & { [PotBrand]: unknown };
-
-const Pot = {
-  new(value: PotState): [Pot, (value: PotState) => void] {
-    let pot = $state({
-      id: value.id,
-      name: value.name,
-    });
-
-    const accessor: PotAccessor = {
-      get id() {
-        return pot.id;
-      },
-      get name() {
-        return pot.name;
-      },
-    };
-
-    const setPot = (value: PotState) => {
-      pot = {
-        id: value.id,
-        name: value.name,
-      };
-    };
-
-    return [accessor as Pot, setPot];
-  },
-};
-
-declare const TabBrand: unique symbol;
-
-type TabAccessor = {
-  views: View[];
-  focusedViewIdx: number;
-};
-export type Tab = TabAccessor & { [TabBrand]: unknown };
-
-const Tab = {
-  new(value: TabState): [Tab, (value: TabState) => void] {
-    let tab = $state({
-      views: value.views.map((v) => View.new(v)[0]),
-      focusedViewIdx: value.focusedViewIdx,
-    });
-
-    const accessor: TabAccessor = {
-      get views() {
-        return tab.views;
-      },
-      get focusedViewIdx() {
-        return tab.focusedViewIdx;
-      },
-    };
-
-    const setTab = (value: TabState) => {
-      tab = {
-        views: value.views.map((v) => View.new(v)[0]),
-        focusedViewIdx: value.focusedViewIdx,
-      };
-    };
-
-    return [accessor as Tab, setTab];
-  },
-};
-
-declare const ViewBrand: unique symbol;
-
-export type ViewAccessor = ViewState;
-type View = ViewAccessor & { [ViewBrand]: unknown };
-
-const View = {
-  new(value: ViewState): [View, (value: ViewState) => void] {
-    let view = $state({
-      id: value.id,
-      viewType: value.viewType,
-      title: value.title,
-      flexGrow: value.flexGrow,
-    });
-
-    const accessor: ViewAccessor = {
-      get id() {
-        return view.id;
-      },
-      get viewType() {
-        return view.viewType;
-      },
-      get title() {
-        return view.title;
-      },
-      get flexGrow() {
-        return view.flexGrow;
-      },
-    };
-
-    const setView = (value: ViewState) => {
-      view = {
-        id: value.id,
-        viewType: value.viewType,
-        title: value.title,
-        flexGrow: value.flexGrow,
-      };
-    };
-
-    return [accessor as View, setView];
-  },
-};
-
-declare const SidebarBrand: unique symbol;
-
-type SidebarAccessor = SidebarState & {
-  resize: (width: number) => void;
-  toggleFloat: () => void;
-};
-
-export type Sidebar = SidebarAccessor & {
-  [SidebarBrand]: unknown;
-};
-
-const Sidebar = {
-  new(value: SidebarState): [Sidebar, (value: SidebarState) => void] {
-    let sidebar = $state({
-      isFloat: value.isFloat,
-      width: value.width,
-    });
-
-    const accessor: SidebarAccessor = {
-      get isFloat() {
-        return sidebar.isFloat;
-      },
-      get width() {
-        return sidebar.width;
-      },
-      resize(width: number) {
-        sidebar.width = width;
-      },
-      toggleFloat() {
-        sidebar.isFloat = !sidebar.isFloat;
-      },
-    };
-
-    const setSidebar = (value: SidebarState) => {
-      sidebar = {
-        isFloat: value.isFloat,
-        width: value.width,
-      };
-    };
-
-    return [accessor as Sidebar, setSidebar];
+    return getContext<WorkspaceState>(key);
   },
 };
