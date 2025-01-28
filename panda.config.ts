88b9a760bb9a4ebdaebdb5b95ddcca053c0a6c89
Joichiro Hayashi <jyoichirou99@gmail.com>
2025-01-28 19:54:18 +0900

    feat: add tokens to panda.config.ts

--- panda.config.ts
+++ panda.config.ts
@@ -27,18 +27,148 @@ export default defineConfig({
     },
     semanticTokens: {
       colors: {
-        sub: {
-          base: {
+        selected: {
+          value: {
+            base: "rgb(0 0 0 / 0.05)",
+            _dark: "rgb(255 255 255 / 0.05)",
+          },
+        },
+        view: {
+          bg: {
             value: {
-              base: "#000000",
-              _dark: "#BF996A",
+              base: "#F7F7F7",
+              _dark: "#242424",
             },
           },
-          selected: {
+          text: {
             value: {
-              base: "#000000",
-              _dark: "#BF996A",
+              base: "#0C0A09",
+              _dark: "#F8F8F8",
             },
+          },
+          "text-muted": {
+            value: {
+              base: "#78716C",
+              _dark: "C7C7C7",
+            },
+          },
+        },
+        card: {
+          bg: {
+            value: {
+              base: "#FFFFFF",
+              _dark: "#333333",
+            },
+          },
+          text: {
+            value: {
+              base: "#0C0A09",
+              _dark: "#F8F8F8",
+            },
+          },
+          "text-muted": {
+            value: {
+              base: "#78716C",
+              _dark: "C7C7C7",
+            },
+          },
+        },
+        button: {
+          bg: {
+            value: {
+              base: "#FFFFFF",
+              _dark: "#333333",
+            },
+          },
+          text: {
+            value: {
+              base: "#0C0A09",
+              _dark: "#F8F8F8",
+            },
+          },
+          "text-muted": {
+            value: {
+              base: "#78716C",
+              _dark: "C7C7C7",
+            },
+          },
+        },
+        accent: {
+          bg: {
+            value: {
+              base: "#A39E93",
+              _dark: "#525252",
+            },
+          },
+          text: {
+            value: {
+              base: "#FEFEFE",
+              _dark: "#F9F9F9",
+            },
+          },
+          "text-muted": {
+            value: {
+              base: "#78716C",
+              _dark: "9D948D",
+            },
+          },
+        },
+      },
+      radii: {
+        md: {
+          value: "0.5rem",
+        },
+        lg: {
+          value: "1rem",
+        },
+      },
+      shadows: {
+        sm: {
+          value: {
+            offsetX: 0,
+            offsetY: 1,
+            blur: 1,
+            spread: 0,
+            color: "rgb(0 0 0 / 0.25)",
+          },
+        },
+        md: {
+          DEFAULT: {
+            value: {
+              offsetX: 0,
+              offsetY: 4,
+              blur: 4,
+              spread: 0,
+              color: "rgb(0 0 0 / 0.25)",
+            },
+          },
+          around: {
+            value: {
+              offsetX: 0,
+              offsetY: 0,
+              blur: 4,
+              spread: 0,
+              color: "rgb(0 0 0 / 0.25)",
+            },
+          },
+          inner: {
+            value: {
+              offsetX: 0,
+              offsetY: 0,
+              blur: 3,
+              spread: 0,
+              color: "rgb(0 0 0 / 0.60)",
+              inset: true,
+            },
+          },
+        },
+        lg: {
+          value: {
+            offsetX: 0,
+            offsetY: 2,
+            blur: 8,
+            spread: 0,
+            color: "rgb(0 0 0 / 0.40)",
           },
         },
       },
