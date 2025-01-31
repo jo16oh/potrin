99489bd6bc8986faec2232b78f42232380b19c77
Joichiro Hayashi <jyoichirou99@gmail.com>
2025-01-31 12:32:01 +0900

    feat: UserIcon component

---
+++ src/lib/components/UserIcon.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { UserRound } from "lucide-svelte";
+  import { css, type Styles } from "styled-system/css";
+  import type { Snippet } from "svelte";
+  let { style }: { style?: Styles; children?: Snippet } = $props();
+</script>
+
+<div
+  class={css(
+    {
+      rounded: "circle",
+      bg: "selected",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      w: "6",
+      h: "6",
+    },
+    style,
+  )}
+>
+  <UserRound class={css({ w: "4", h: "4", color: "button.text-muted" })} />
+</div>
