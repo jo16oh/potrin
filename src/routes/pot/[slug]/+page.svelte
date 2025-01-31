ee0c4e80677c9b44858077b768de872d2cb6f2e5
Joichiro Hayashi <jyoichirou99@gmail.com>
2025-01-31 12:38:39 +0900

    chore: remove tailwind related codes

--- src/routes/pot/[slug]/+page.svelte
+++ src/routes/pot/[slug]/+page.svelte
@@ -1,23 +1,8 @@
 <script lang="ts">
-  // import CreateButton from "$lib/components/CreateButton.svelte";
-  // import Editor from "$lib/components/Editor.svelte";
-  // import Sidebar from "$lib/components/Sidebar.svelte";
   import { App } from "$lib/models/App.svelte";
   import { css } from "styled-system/css";
 
-  const app = App.state();
+  const [appState, updateAppState] = App.state();
 </script>
 
-<div class={css({ color: "sub.base" })}>Hello 🐼!</div>
-<!-- <Sidebar> -->
-<!--   <div class="h-6 w-full bg-black"> -->
-<!--     <button class="h-full w-96 bg-white" onclick={() => console.log("click")} -->
-<!--       >click me</button -->
-<!--     > -->
-<!--   </div> -->
-<!--   <!-- <Editor /> --> -->
-<!--   <!-- <CreateButton /> --> -->
-<!--   {#each Object.values(app.pots) as pot_name} -->
-<!--     <div>{pot_name}</div> -->
-<!--   {/each} -->
-<!-- </Sidebar> -->
+<div class={css({ color: "button.text-muted" })}>Hello 🐼!</div>
