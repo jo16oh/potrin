6922d44c4f713e80ea488eb7c3cf3b034ad78b03
Joichiro Hayashi <jyoichirou99@gmail.com>
2025-01-31 12:28:40 +0900

    feat: Button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  import { Button } from "bits-ui";
+  import { css } from "styled-system/css";
+  import type { Styles } from "styled-system/css";
+  import type { Snippet } from "svelte";
+
+  let {
+    style,
+    disabled = $bindable(),
+    children,
+    ...others
+  }: {
+    style?: Styles;
+    disabled?: boolean;
+    children: Snippet;
+    onclick?: () => void;
+  } = $props();
+</script>
+
+<Button.Root
+  class={css(
+    {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: "center",
+      gap: "2",
+      paddingX: "4",
+      paddingY: "2",
+      bg: "button.bg",
+      shadow: "sm",
+      rounded: "lg",
+      width: "fit",
+      height: "fit",
+      _hover: {
+        bg: "selected",
+      },
+      _disabled: {
+        color: "button.text-muted",
+        bg: "button.bg/20",
+        _hover: {
+          bg: "button.bg/20",
+        },
+      },
+      transition: "colors",
+    },
+    style,
+  )}
+  {disabled}
+  {...others}
+>
+  {@render children?.()}</Button.Root
+>
