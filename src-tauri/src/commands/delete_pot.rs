5c4b606d2ab3a4947e09fe526f04e37885087c0d
Joichiro Hayashi <jyoichirou99@gmail.com>
2025-02-01 12:02:17 +0900

    fix: close the window when deleting a pot

--- src-tauri/src/commands/delete_pot.rs
+++ src-tauri/src/commands/delete_pot.rs
@@ -1,7 +1,7 @@
 use crate::types::util::UUIDv7Base64URL;
 use crate::{database::query, utils::get_state};
 use sqlx::SqlitePool;
-use tauri::{AppHandle, Runtime};
+use tauri::{AppHandle, Manager, Runtime};
 
 #[tauri::command]
 #[specta::specta]
@@ -14,6 +14,10 @@ pub async fn delete_pot<R: Runtime>(
     let pool = get_state::<R, SqlitePool>(&app_handle)?;
 
     query::delete::pot(pool, pot_id).await?;
+
+    if let Some(win) = app_handle.webview_windows().get(&pot_id.to_string()) {
+        win.close()?;
+    }
 
     eyre::Ok(())
 }
