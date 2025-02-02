6e66a71174782035f52f3052304a37a5f1699de7
Joichiro Hayashi <jyoichirou99@gmail.com>
2025-02-02 13:59:00 +0900

    fix: load workspace state correctly

--- src/routes/pot/+layout.ts
+++ src/routes/pot/+layout.ts
@@ -1,8 +1,9 @@
 import type { LayoutLoad } from "./$types";
 import { commands } from "../../generated/tauri-commands";
+import { unwrap } from "$lib/utils";
 
 export const load: LayoutLoad = async () => {
   return {
-    workspaceState: await commands.getWorkspaceState(),
+    workspaceState: unwrap(await commands.getWorkspaceState()),
   };
 };
