ff96b6bcadddbc4f69dba1457b9bba6c1156030b
Joichiro Hayashi <jyoichirou99@gmail.com>
2025-02-06 16:54:44 +0900

    feat: scaffolding CardsView.svelte

---
+++ src/lib/components/view/CardsView.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+  import { X } from "lucide-svelte";
+  import { css } from "styled-system/css";
+  import DialogClose from "../common/DialogClose.svelte";
+  import { buttonStyle } from "../common/Button.svelte";
+</script>
+
+<div class={containerStyle}>
+  <div class={headerStyle}>
+    <div class={headerLeftButtons}></div>
+    <div class={headerRightButtons}>
+      <DialogClose class={headerButtonStyle}>
+        <X class={headerIconStyle} />
+      </DialogClose>
+    </div>
+  </div>
+  <div class={contentContainerStyle}>content</div>
+</div>
+
+<script module>
+  const containerStyle = css({
+    h: "fit",
+    w: "full",
+  });
+
+  const headerStyle = css({
+    display: "flex",
+    flexDir: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    w: "full",
+    h: "8",
+    px: "1",
+    py: "1",
+  });
+
+  const headerLeftButtons = css({
+    display: "flex",
+    flexDir: "row",
+    w: "fit",
+  });
+
+  const headerRightButtons = css({
+    display: "flex",
+    flexDir: "row",
+    w: "fit",
+  });
+
+  const headerButtonStyle = css({
+    ...buttonStyle,
+    p: "0",
+    w: "6",
+    h: "6",
+    rounded: "circle",
+    shadow: "[none]",
+    bg: "transparent",
+    _hover: {
+      bg: "selected",
+    },
+  });
+
+  const headerIconStyle = css({
+    w: "4",
+    h: "4",
+    color: "view.text-muted",
+  });
+
+  const contentContainerStyle = css({
+    px: "2",
+    py: "24",
+    display: "flex",
+    flexDir: "column",
+  });
+</script>
--- src/routes/pot/[slug]/+page.svelte
+++ src/routes/pot/[slug]/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+  import Dialog from "$lib/components/common/Dialog.svelte";
   import TitleBarHandler from "$lib/components/common/TitleBarHandler.svelte";
   import Sidebar from "$lib/components/sidebar/Sidebar.svelte";
+  import CardsView from "$lib/components/view/CardsView.svelte";
+  import { PencilLine } from "lucide-svelte";
   import { css } from "styled-system/css";
 </script>
 
@@ -8,6 +11,35 @@
 
 <div class={containerStyle}>
   <Sidebar />
+  <div
+    class={css({
+      bg: "view.bg",
+      w: "full",
+      h: "full",
+      rounded: "md",
+      shadow: "md.around",
+    })}
+  ></div>
+  <div
+    class={css({
+      bg: "view.bg",
+      w: "full",
+      h: "full",
+      rounded: "md",
+      shadow: "md.around",
+    })}
+  ></div>
+  <Dialog
+    triggerStyle={floatingButtonStyle}
+    contentStyle={hoverViewContainerStyle}
+  >
+    {#snippet trigger()}
+      <PencilLine class={floatingButtonIconStyle} />
+    {/snippet}
+    {#snippet content()}
+      <CardsView />
+    {/snippet}
+  </Dialog>
 </div>
 
 <script module>
@@ -21,5 +53,37 @@
     pt: "[28px]",
     pb: "[8px]",
     px: "[8px]",
+  });
+
+  const floatingButtonStyle = css.raw({
+    position: "fixed",
+    right: "[16px]",
+    bottom: "[16px]",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    p: "0",
+    w: "14",
+    h: "14",
+    bg: "accent.bg/90",
+    rounded: "[100%]",
+    transition: "colors",
+    _hover: {
+      bg: "selected",
+    },
+  });
+
+  const floatingButtonIconStyle = css({
+    w: "6",
+    h: "6",
+    color: "[white]",
+  });
+
+  const hoverViewContainerStyle = css.raw({
+    w: "[90vw]",
+    h: "fit",
+    maxW: "[37.5rem]",
+    maxH: "[95vh]",
+    p: "0",
   });
 </script>
