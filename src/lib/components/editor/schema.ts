aea97b7047e63dc07d6b3f0e282a3a21a821f3fe
Joichiro Hayashi <jyoichirou99@gmail.com>
2025-02-14 22:56:39 +0900

    feat: extract/insert contents when Bs/Enter pressed

--- src/lib/components/editor/schema.ts
+++ src/lib/components/editor/schema.ts
@@ -3,6 +3,11 @@ import { Document } from "@tiptap/extension-document";
 import { Paragraph as ParagraphSchema } from "@tiptap/extension-paragraph";
 import { Text } from "@tiptap/extension-text";
 import Collabolation from "@tiptap/extension-collaboration";
+import {
+  countSizeOfYXMLFragment as sizeOfYXMLFragment,
+  insertJSONContentsToYXMLFragment,
+  type FocusPosition,
+} from "./utils";
 import type { Outline } from "$lib/models/Outline.svelte";
 import { Plugin, PluginKey } from "@tiptap/pm/state";
 // @ts-expect-error workaround for https://github.com/sveltejs/kit/issues/7805
@@ -23,8 +28,9 @@ export const createParagraphSchema = async (
   paragraph: ParagraphModel,
   updateFocusPosition: (pos: FocusPosition) => void,
 ) => {
-  const ydoc = await paragraph.ydoc();
-  const fragment = ydoc.getMap("potrin").get("doc") as Y.XmlFragment;
+  const yDoc = await paragraph.ydoc();
+  const yMap = yDoc.getMap("potrin");
+  const fragment = yMap.get("doc") as Y.XmlFragment;
 
   return [
     Document,
@@ -41,23 +47,32 @@ export const createParagraphSchema = async (
             key: new PluginKey("KeydownHandler"),
             props: {
               handleKeyDown(view, event) {
-                const editorRect = view.dom.getBoundingClientRect();
-                const cursorRect = view.coordsAtPos(view.state.selection.from);
+                const state = view.state;
+                const doc = state.doc;
+                const selection = state.selection;
+                const { $from } = state.selection;
+
                 const REM = 16;
 
+                const isDocEmpty = doc.content.size <= 2;
+                const isCursorAtDocStart = selection.from === 1;
+                const isPreviousNodeEmpty = !$from.nodeBefore;
+                const isCursorAtNodeStart = $from.parentOffset === 0;
+
                 if (event.key === "Enter") {
-                  const currentNode = view.state.selection.$from.node();
-                  const isDocEmpty = view.state.doc.content.size <= 2;
-                  const isCurrentNodeEmpty =
-                    currentNode.content.size === 0 &&
-                    currentNode.textContent.length === 0;
+                  if (isCursorAtDocStart) return true;
+                  if (!isPreviousNodeEmpty || !isCursorAtNodeStart)
+                    return false;
 
-                  if (isDocEmpty || !isCurrentNodeEmpty) return false;
+                  view.dom.blur();
 
-                  const { $from } = view.state.selection;
-                  const node = $from.node();
-                  const pos = $from.before();
-                  view.dispatch(view.state.tr.delete(pos, pos + node.nodeSize));
+                  const end = doc.content.size;
+
+                  const content = doc
+                    .slice(selection.from, end)
+                    .toJSON().content;
+
+                  view.dispatch(state.tr.delete($from.pos - 1, end));
 
                   const paragraphAfter =
                     paragraph.outline!.paragraphs[
@@ -72,88 +87,136 @@ export const createParagraphSchema = async (
                       paragraph.fractionalIndex,
                       paragraphAfter ? paragraphAfter.fractionalIndex : null,
                     ),
-                  );
+                  ) as ParagraphModel;
 
                   paragraph.outline?.insertParagraph(newParagraph);
 
-                  updateFocusPosition({
-                    id: newParagraph.id,
-                    position: "start",
+                  newParagraph.ydoc().then((ydoc) => {
+                    const fragment = ydoc
+                      .getMap("potrin")
+                      .get("doc") as Y.XmlFragment;
+
+                    insertJSONContentsToYXMLFragment(
+                      content,
+                      state.schema,
+                      fragment,
+                      ydoc,
+                    );
+
+                    updateFocusPosition({
+                      id: newParagraph.id,
+                      position: "start",
+                    });
                   });
 
                   return true;
                 }
 
-                if (
-                  event.key === "Backspace" &&
-                  view.state.selection.from === 2
-                ) {
-                  const yMap = ydoc.getMap("potrin");
-                  yMap.set("deleted", true);
-
+                if (event.key === "Backspace" && selection.from === 1) {
                   const paragraphBeforeIndex =
                     paragraph.outline!.paragraphs.findIndex(
                       (p) => p.id === paragraph.id,
                     ) - 1;
 
-                  const paragraphBefore =
-                    paragraph.outline!.paragraphs[paragraphBeforeIndex];
+                  if (paragraphBeforeIndex !== -1) {
+                    const paragraphBefore =
+                      paragraph.outline!.paragraphs[paragraphBeforeIndex];
 
-                  paragraph.outline?.removeParagraph(paragraph);
+                    paragraphBefore?.ydoc().then((ydocBefore) => {
+                      const fragmentBefore = ydocBefore
+                        .getMap("potrin")
+                        .get("doc") as Y.XmlFragment;
 
-                  updateFocusPosition({
-                    id: paragraphBefore
-                      ? paragraphBefore.id
-                      : paragraph.outline
-                        ? paragraph.outline.id
-                        : null,
+                      const sizeOfFragmentBefore = sizeOfYXMLFragment(
+                        fragmentBefore,
+                        state.schema,
+                      );
 
-                    position: "end",
-                  });
+                      const content = doc.toJSON().content;
 
-                  return false;
-                }
+                      insertJSONContentsToYXMLFragment(
+                        content,
+                        state.schema,
+                        fragmentBefore,
+                        ydocBefore,
+                        false,
+                      );
 
-                if (
-                  event.key === "ArrowUp" &&
-                  cursorRect.top - editorRect.top < REM
-                ) {
-                  const paragraphBefore =
-                    paragraph.outline?.paragraphs[
-                      paragraph.outline?.paragraphs.findIndex(
-                        (p) => p.id === paragraph.id,
-                      ) - 1
-                    ];
+                      paragraph.outline?.removeParagraph(paragraph);
+                      yDoc.transact(() => {
+                        yMap.set("deleted", true);
+                        fragment.delete(1, fragment.length - 1);
+                      });
 
-                  updateFocusPosition({
-                    id: paragraphBefore
-                      ? paragraphBefore.id
-                      : paragraph.outline!.id,
-                    position: "end",
-                  });
+                      updateFocusPosition({
+                        id: paragraphBefore
+                          ? paragraphBefore.id
+                          : paragraph.outline
+                            ? paragraph.outline.id
+                            : null,
+                        position: sizeOfFragmentBefore - 1,
+                      });
+                    });
+                  } else if (isDocEmpty) {
+                    yDoc.transact(() => {
+                      yMap.set("deleted", true);
+                    });
 
-                  return false;
-                }
+                    paragraph.outline?.removeParagraph(paragraph);
 
-                if (
-                  event.key === "ArrowDown" &&
-                  editorRect.bottom - cursorRect.bottom < REM
-                ) {
-                  const paragraphAfter =
-                    paragraph.outline?.paragraphs[
-                      paragraph.outline?.paragraphs.findIndex(
-                        (p) => p.id === paragraph.id,
-                      ) + 1
-                    ];
-
-                  if (paragraphAfter) {
                     updateFocusPosition({
-                      id: paragraphAfter.id,
-                      position: "start",
+                      id: paragraph.outline?.id ?? null,
+                      position: "end",
                     });
                   }
 
                   return false;
+                }
+
+                if (event.key === "ArrowUp") {
+                  const editorRect = view.dom.getBoundingClientRect();
+                  const cursorRect = view.coordsAtPos(selection.from);
+
+                  if (cursorRect.top - editorRect.top < REM) {
+                    const paragraphBefore =
+                      paragraph.outline?.paragraphs[
+                        paragraph.outline?.paragraphs.findIndex(
+                          (p) => p.id === paragraph.id,
+                        ) - 1
+                      ];
+
+                    updateFocusPosition({
+                      id: paragraphBefore
+                        ? paragraphBefore.id
+                        : paragraph.outline!.id,
+                      position: "end",
+                    });
+
+                    return false;
+                  }
+                }
+
+                if (event.key === "ArrowDown") {
+                  const editorRect = view.dom.getBoundingClientRect();
+                  const cursorRect = view.coordsAtPos(selection.from + 1);
+
+                  if (editorRect.bottom - cursorRect.bottom < REM) {
+                    const paragraphAfter =
+                      paragraph.outline?.paragraphs[
+                        paragraph.outline?.paragraphs.findIndex(
+                          (p) => p.id === paragraph.id,
+                        ) + 1
+                      ];
+
+                    if (paragraphAfter) {
+                      updateFocusPosition({
+                        id: paragraphAfter.id,
+                        position: "start",
+                      });
+                    }
+
+                    return false;
+                  }
                 }
 
                 return false;
@@ -188,14 +251,20 @@ export const createOutlineSchema = async (
             key: new PluginKey("KeydownHandler"),
             props: {
               handleKeyDown(view, event) {
+                const state = view.state;
+                const selection = state.selection;
+
                 const editorRect = view.dom.getBoundingClientRect();
-                const cursorRect = view.coordsAtPos(view.state.selection.from);
+                const cursorRect = view.coordsAtPos(selection.from);
                 const REM = 16;
 
                 if (event.key === "Enter") {
                   const newParagraph = Paragraph.new(
                     outline,
-                    generateKeyBetween(null, null),
+                    generateKeyBetween(
+                      null,
+                      outline.paragraphs[0]?.fractionalIndex ?? null,
+                    ),
                   );
 
                   outline.insertParagraph(newParagraph);
@@ -208,10 +277,7 @@ export const createOutlineSchema = async (
                   return true;
                 }
 
-                if (
-                  event.key === "Backspace" &&
-                  view.state.selection.from === 2
-                ) {
+                if (event.key === "Backspace" && selection.from === 2) {
                   return false;
                 }
 
@@ -219,20 +285,6 @@ export const createOutlineSchema = async (
                   event.key === "ArrowUp" &&
                   cursorRect.top - editorRect.top < REM
                 ) {
-                  // const paragraphBefore =
-                  //   paragraph.outline!.paragraphs[
-                  //     paragraph.outline!.paragraphs.findIndex(
-                  //       (p) => p.id === paragraph.id,
-                  //     ) - 1
-                  //   ];
-                  //
-                  // updateFocusPosition({
-                  //   id: paragraphBefore
-                  //     ? paragraphBefore.id
-                  //     : paragraph.outline!.id,
-                  //   position: "end",
-                  // });
-
                   return false;
                 }
 
--- src/lib/components/editor/utils.ts
+++ src/lib/components/editor/utils.ts
@@ -1,4 +1,13 @@
-import type { FocusPosition as TiptapFocusPosition } from "@tiptap/core";
+import type {
+  JSONContent,
+  FocusPosition as TiptapFocusPosition,
+} from "@tiptap/core";
+import type { Schema } from "@tiptap/pm/model";
+import {
+  prosemirrorJSONToYXmlFragment,
+  yXmlFragmentToProseMirrorRootNode,
+} from "y-prosemirror";
+import * as Y from "yjs";
 
 export type EditorFocusPosition = TiptapFocusPosition;
 
@@ -6,3 +15,49 @@ export type FocusPosition = {
   id: string | null;
   position: EditorFocusPosition;
 };
+
+export function insertJSONContentsToYXMLFragment(
+  content: JSONContent[],
+  schema: Schema,
+  fragment: Y.XmlFragment,
+  ydoc: Y.Doc,
+  appendToLastNode?: boolean,
+) {
+  const doc = {
+    type: "doc",
+    content: content,
+  };
+
+  const fragmentToInsert = new Y.Doc().getXmlFragment();
+  prosemirrorJSONToYXmlFragment(schema, doc, fragmentToInsert);
+
+  const lastNode = fragment.get(fragment.length - 1);
+
+  ydoc.transact(() => {
+    fragmentToInsert.forEach((e, i) => {
+      if (i === 0 && e.length === 0) {
+        return;
+      } else if (
+        appendToLastNode &&
+        i === 0 &&
+        lastNode instanceof Y.XmlElement &&
+        e instanceof Y.XmlElement &&
+        lastNode.nodeName === e.nodeName
+      ) {
+        e.forEach((e) => {
+          lastNode.insert(lastNode.length, [e.clone()]);
+        });
+      } else {
+        fragment.insert(fragment.length, [e.clone()]);
+      }
+    });
+  });
+}
+
+export function countSizeOfYXMLFragment(
+  fragment: Y.XmlFragment,
+  schema: Schema,
+): number {
+  const doc = yXmlFragmentToProseMirrorRootNode(fragment, schema);
+  return doc.nodeSize;
+}
