539155a113f35a309e3e57fdeb936545864c39d9
Joichiro Hayashi <jyoichirou99@gmail.com>
2025-03-06 00:53:33 +0900

    add vertical line svgs

---
+++ src/lib/components/icon/VerticalLine.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import type { SVGAttributes } from "svelte/elements";
+  let { ...restProps }: SVGAttributes<SVGElement> = $props();
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" {...restProps}>
+  <line
+    y1="0"
+    y2="100%"
+    stroke="currentColor"
+    stroke-width="0.125rem"
+    stroke-linecap="round"
+  />
+</svg>
