5b80c8c3dde7ef78ddfb71fe0698a120db472f37
Joichiro Hayashi <jyoichirou99@gmail.com>
2025-03-14 17:26:21 +0900

    extract ViewHistoryNavigation component

---
+++ src/lib/components/view/common/ViewHistoryNavigation.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import Button from "$lib/components/common/Button.svelte";
+  import { View } from "$lib/models/Workspace.svelte";
+  import { ChevronLeft, ChevronRight } from "lucide-svelte";
+  import { css, type Styles } from "styled-system/css";
+
+  type Props = {
+    view: View;
+    buttonStyle: Styles;
+    iconStyle: Styles;
+  };
+
+  let { view, buttonStyle, iconStyle }: Props = $props();
+</script>
+
+<Button
+  class={css(buttonStyle)}
+  disabled={!View.hasPrev(view.id)}
+  onmousedown={(e: MouseEvent) => e.preventDefault()}
+  onclick={(e: MouseEvent) => {
+    e.preventDefault();
+    View.back(view);
+  }}
+>
+  <ChevronLeft class={css(iconStyle)} data-disabled={!View.hasPrev(view.id)} />
+</Button>
+<Button
+  class={css(buttonStyle)}
+  disabled={!View.hasNext(view.id)}
+  onmousedown={(e: MouseEvent) => e.preventDefault()}
+  onclick={(e: MouseEvent) => {
+    e.preventDefault();
+    View.forward(view);
+  }}
+>
+  <ChevronRight class={css(iconStyle)} data-disabled={!View.hasNext(view.id)} />
+</Button>
